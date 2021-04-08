78e27dbca297424185932332b895e962bfd0637c
m-const <55905384+m-const@users.noreply.github.com>
2021-04-07 23:32:10 -0400

    d

--- build_JSAuth.sh
+++ build_JSAuth.sh
@@ -1,6 +1,6 @@
-#!/bin/sh
+#!/bin/bash
 
-echo "Running script: ${0} in: $( pwd )"
+echo "Running script: ${0} in: $( pwd ) using ${ which bash }"
 
 #copy required files to the dist DIR. Assume provided build DIRS are ALL required, only specified root files are included
 BUILD_DIRS="routes utils models"
